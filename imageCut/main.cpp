0a2a9ed2dac6f6025afbba092a65dd703c250eb3
ZENJU Daisuke <yoshizumi@zoho.com>
2015-09-19 01:41:06 +0900

    Initial Commit

---
+++ imageCut/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  imageCut
+//
+//  Created by ZENJU Daisuke on 2015/09/19.
+//  Copyright © 2015年 ZENJU Daisuke. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
